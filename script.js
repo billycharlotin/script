ce16dd8cccb1a32cb789f18e81b42467abf659a1
billy <charlotinbilly390@gmail.com>
2026-03-10 02:36:56 +0100

    modification du code: ajout du methode statique et instance get et set

--- script.js
+++ script.js
@@ -1,48 +1,70 @@
-// ORDINATEU LE NOM DE MA CCLASS
-class ordinateu {
-  //CONSTRUCTEUR PERMET DE  CREER DES OBJET AVEC THIS
-  constructor(marque, ram, giga) {
-    this.marque = marque;
-    this.giga = giga;
-    this.ram = ram;
+class voiture {
+  constructor(marque, couleur, année) {
+    this._marque = marque;
+    this._couleur = couleur;
+    this._année = année;
   }
 
-  //  PROPRIETE STATIC  ON L APPEL AVEC LE NOM DE LA CLASS
-  static appareil_pris_en_charge = "ordinateur";
-  //PRIETE INSTANCE
-  type = "laptop";
-  version = 2026;
-  // METHODE STATIC OU DE CLASS
-  static définition() {
-    console.log("les ordinateurs sont des matériels de haute gamme ");
-  }
+  static type_De_Voiture = " vehicule hybride";
 
-  // METHODE D INSTANCE
-  mon_ordi_luxe() {
+  static radar_De_Vitesse() {
     console.log(
-      " les ordinateur avec  le logo pomme sont considérés comme des ordinateur de luxe"
+      `Tous nos vehicules sont équipés d'une option limitateur de vitesse  `
     );
   }
 
-  //METHODE D INSTANCE
-  ficheProduit() {
+  info() {
     console.log(
-      ` la marque est: ${this.marque} nombre de giga:${this.giga}  produit de luxe.`
+      `,nous vous proposons des voitures hybrides avec une remise de 2000 euro sur la marque  ${this._marque} `
     );
+  }
+  demarer() {
+    console.log(` ${this._marque}, démarage rapide automatique`);
+  }
+
+  get marque() {
+    return this._marque;
+  }
+  set marque(value) {
+    this._marque = value;
   }
 }
-// INITIALISTATION
-let mon_ordi1 = new ordinateu("mctouch", 500, 16);
-let mon_ordi2 = new ordinateu("lenovo", 250, 8);
+let V1 = new voiture("bmw", "gris_nardo", 2026);
 
-console.log(ordinateu.appareil_pris_en_charge);
+console.log(V1._marque);
+voiture.radar_De_Vitesse();
 
-ordinateu.définition();
+V1.marque = "audi";
 
-console.log(
-  ` la marque est : ${mon_ordi1.marque}, la ram est de ${mon_ordi1.ram} et le nombre de giga est ${mon_ordi1.giga}`
-);
-mon_ordi1.mon_ordi_luxe();
-mon_ordi1.ficheProduit();
-console.log(mon_ordi1.type);
-console.log(mon_ordi2.type);
+console.log(V1._année);
+console.log(V1._couleur);
+console.log(V1._marque);
+
+console.log(voiture.type_De_Voiture);
+V1.demarer();
+V1.info();
+
+// J'ai crée une class banque avec  nom et solde
+class banque {
+  // Constructeur
+  constructor(nom, solde) {
+    this._nom = nom;
+    this._solde = solde;
+  }
+  //Utilisation de getters
+  get solde() {
+    return this._solde;
+  }
+  //Utilisation de setters
+  set solde(value) {
+    this._solde = value;
+  }
+}
+//initialisation  instance avec le constructeur
+let client = new banque("billy", 5000);
+// Appélation du nom  ET solde à l ' aide de l'instance cleint
+console.log(client._nom);
+console.log(client._solde);
+// j'ai un nouveau solde toujour avec de l'instance client
+client._solde = 12000;
+console.log(client._solde);
